b18a505f7a06d8a0dfeb23edc9e32209c5181a94
chenzdido <chenzdido@gmail.com>
2020-09-16 22:42:53 +0800

    [zhe] feat:resolve circular dependencies.

--- src/main/java/com/thoughtworks/capability/demospringioccontainer/Bar.java
+++ src/main/java/com/thoughtworks/capability/demospringioccontainer/Bar.java
@@ -1,14 +1,35 @@
 package com.thoughtworks.capability.demospringioccontainer;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
 
 @Component
 public class Bar {
 
     private Foo foo;
 
+    //方法1
+    /*public Bar(@Lazy Foo foo) {
+        this.foo = foo;
+    }*/
+
     public Bar(Foo foo) {
         this.foo = foo;
+    }
+
+    //方法2
+    /*@Autowired
+    public void setFoo(Foo foo) {
+        this.foo = foo;
+    }*/
+
+    //方法3
+    @PostConstruct
+    public void init() {
+        foo.setBar(this);
     }
 
     public void hi() {
--- src/main/java/com/thoughtworks/capability/demospringioccontainer/Foo.java
+++ src/main/java/com/thoughtworks/capability/demospringioccontainer/Foo.java
@@ -1,13 +1,27 @@
 package com.thoughtworks.capability.demospringioccontainer;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
 
 @Component
 public class Foo {
 
     private Bar bar;
 
-    public Foo(Bar bar) {
+    /*public Foo(Bar bar) {
+        this.bar = bar;
+    }*/
+
+    //方法2
+    /*@Autowired
+    public void setBar(Bar bar) {
+        this.bar = bar;
+    }*/
+
+    //方法3
+    public void setBar(Bar bar) {
         this.bar = bar;
     }
 
