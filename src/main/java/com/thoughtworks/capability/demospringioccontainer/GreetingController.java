70e6f41b664411c8f07d370f18d475937ed104d1
chenzdido <chenzdido@gmail.com>
2020-09-16 22:46:51 +0800

    [zhe] feat:resolve create new greetingService bean.

--- src/main/java/com/thoughtworks/capability/demospringioccontainer/GreetingController.java
+++ src/main/java/com/thoughtworks/capability/demospringioccontainer/GreetingController.java
@@ -5,6 +5,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+//方法1
+//@Scope(value=SCOPE_PROTOTYPE)
 public class GreetingController {
 
     private final GreetingService greetingService;
--- src/main/java/com/thoughtworks/capability/demospringioccontainer/GreetingService.java
+++ src/main/java/com/thoughtworks/capability/demospringioccontainer/GreetingService.java
@@ -3,6 +3,10 @@ package com.thoughtworks.capability.demospringioccontainer;
 import org.springframework.stereotype.Service;
 
 @Service
+//方法1
+//@Scope(value=SCOPE_PROTOTYPE)
+//方法2
+//@RequestScope
 public class GreetingService {
 
     String sayHi() {
